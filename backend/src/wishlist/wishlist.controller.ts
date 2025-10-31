c2a2cdafe6f5a0e5de5e914d8a27b555a054435a
AliReda2 <alireda2572003@gmail.com>
2025-10-31 20:43:30 +0200

    add wishlist

---
+++ backend/src/wishlist/wishlist.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Delete,
+  Param,
+  UseGuards,
+} from '@nestjs/common';
+import { WishlistService } from './wishlist.service';
+import type { User } from '@prisma/client';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { AtGuard } from 'src/auth/guard';
+import { GetUser } from 'src/auth/decorator';
+
+@ApiTags('Wishlist')
+@ApiBearerAuth('access-token')
+@UseGuards(AtGuard)
+@Controller('wishlist')
+export class WishlistController {
+  constructor(private wishlistService: WishlistService) {}
+
+  @Get()
+  @ApiOperation({ summary: 'Get user wishlist' })
+  async getWishlist(@GetUser() user: User) {
+    return this.wishlistService.getWishlist(user);
+  }
+
+  @Post(':productId')
+  @ApiOperation({ summary: 'Add a product to wishlist' })
+  async addToWishlist(
+    @GetUser() user: User,
+    @Param('productId') productId: string,
+  ) {
+    return this.wishlistService.addToWishlist(user, productId);
+  }
+
+  @Delete(':productId')
+  @ApiOperation({ summary: 'Remove a product from wishlist' })
+  async removeFromWishlist(
+    @GetUser() user: User,
+    @Param('productId') productId: string,
+  ) {
+    return this.wishlistService.removeFromWishlist(user, productId);
+  }
+
+  @Delete()
+  @ApiOperation({ summary: 'Clear all items from wishlist' })
+  async clearWishlist(@GetUser() user: User) {
+    return this.wishlistService.clearWishlist(user);
+  }
+}
