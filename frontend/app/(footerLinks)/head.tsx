7a8e080d7a25dc1439f401a25fd2d92c14dc652f
AliReda2 <alireda2572003@gmail.com>
2025-11-18 20:19:53 +0200

    update Footer UI 3

---
+++ frontend/app/(footerLinks)/head.tsx
@@ -0,0 +1,20 @@
+export default function Head() {
+  return (
+    <>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+      />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="anonymous"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+        rel="stylesheet"
+      />
+    </>
+  );
+}
