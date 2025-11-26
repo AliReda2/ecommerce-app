ac1f650ec295d1abd091f66295bbdb30e8aac123
MohammadYahfoufi <mhmdyahfoufi52@gmail.com>
2025-11-26 19:50:59 +0200

    update cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.use(cookieParser());
 
   // Allow cookies to be sent from frontend. Use specific origin when possible
-  const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3001';
+  const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3001' || 'http://127.0.0.1:5500';
   app.enableCors({
     origin: [
       'https://ecommerce-app-ten-blush.vercel.app',
