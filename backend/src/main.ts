a29ab830073b17aa0ddc23dd5b35de3e0aead3c3
Ali Reda <alireda2572003@gmail.com>
2025-11-29 19:59:26 +0200

    add helmet security

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,25 +2,38 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api');
-  // enable cookie parsing so strategies/controllers can read cookies
+
+  // Parse cookies
   app.use(cookieParser());
 
+  // Helmet security headers
+  app.use(
+    helmet({
+      crossOriginResourcePolicy: false, // Important for Swagger & images
+      crossOriginOpenerPolicy: false, // Avoids breaking Swagger UI
+      crossOriginEmbedderPolicy: false, // Avoids blocking embeds
+    }),
+  );
 
+  // CORS
   app.enableCors({
     origin: [
       'https://shopcodartlb.vercel.app',
       'https://shopcodart.vercel.app',
       'http://localhost:3001',
-      'http://127.0.0.1:5500'
+      'http://127.0.0.1:5500',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
 
+  // Swagger
   const config = new DocumentBuilder()
     .setTitle('E-Commerce API')
     .setDescription('API documentation for the E-Commerce application')
@@ -28,6 +41,7 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
